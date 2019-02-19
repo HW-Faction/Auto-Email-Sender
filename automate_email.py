12be5fd4d0b2cc6eaf8607a545ee4d596ce47288
HW-Faction <hankwilliamson49@gmail.com>
2019-02-19 11:22:03 +0530

    v1.0.0

---
+++ automate_email.py
@@ -0,0 +1,64 @@
+import smtplib
+import io
+
+from string import Template
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import credentials
+
+
+def get_contacts(filename):
+    names = []
+    emails = []
+    with io.open(filename, encoding='utf-8') as contacts_file:
+        for contact in contacts_file:
+            names.append(contact.split()[0])
+            emails.append(contact.split()[1])
+    return names, emails
+
+
+def read_template(filename):
+   
+    with io.open(filename, encoding='utf-8') as template_file:
+        template = template_file.read()
+    return Template(template)
+
+
+def main():
+    # get names and contact list
+    names, emails = get_contacts('my_contacts.txt')
+    message_template = read_template('message.txt')
+
+    # setup the SMTP server
+    server = smtplib.SMTP(credentials.HOST_NAME, credentials.PORT)
+    server.ehlo()
+    server.starttls()
+    server.login(credentials.EMAIL_ADDRESS, credentials.PASSWORD)
+
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()  # create a message
+
+        # add name to the title
+        message = message_template.substitute(PERSON_NAME=name.title())
+
+        # setup the message parameters
+        msg['From'] = credentials.ADDRESS
+        msg['To'] = email
+        msg['Subject'] = 'Automated Email Sending with Python' # change subject here
+
+        # add message body
+        msg.attach(MIMEText(message, 'plain'))
+
+        # send the message via the server setup earlier
+        server.send_message(msg)
+        del msg
+
+    # close the SMTP session and close the connection
+    server.quit()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ credentials.py
@@ -0,0 +1,4 @@
+HOST_NAME = 'smtp.gmail.com'
+PORT = 587
+EMAIL_ADDRESS = 'Your_Email'
+PASSWORD = 'Your_Password'
